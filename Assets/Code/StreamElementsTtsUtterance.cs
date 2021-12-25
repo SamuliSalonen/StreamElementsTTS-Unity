32c517b97a79b4c0801312352e483bc184821c72
Samuli <salonen.samuli1@gmail.com>
2021-12-25 16:16:25 +0200

    talking hand

--- Assets/Code/StreamElementsTtsUtterance.cs
+++ Assets/Code/StreamElementsTtsUtterance.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 namespace StreamElementsTTS_Unity
 {
     public class StreamElementsTtsUtterance : MonoBehaviour
@@ -17,6 +18,40 @@ namespace StreamElementsTTS_Unity
             Debug.Log("Sock over it?");
             StartCoroutine(StreamElementsTTSApi.SpeakRoutine(text, voice, audioSource));
             //audioSource.Play();
+        }
+
+        float[] spectrum = new float[256];
+
+        public event Action<bool> onSpeakFrame;
+        public float biggestView;
+        private void Update()
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+                float biggest = -1;
+                for (int i = 1; i < spectrum.Length - 1; i++)
+                {
+                    Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
+                    Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
+                    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
+                    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+                    if (biggest < spectrum[i - 1])
+                    {
+                        biggest = spectrum[i - 1];
+                    }
+                }
+
+                biggestView = biggest;
+                onSpeakFrame?.Invoke(biggest > 0.05f);
+            
+            }
+            else
+            {
+                onSpeakFrame?.Invoke(false);
+
+            }
+            //print(biggest);
         }
     }
 }
---
+++ Assets/Code/TalkingSprite.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace StreamElementsTTS_Unity
+{
+    public class TalkingSprite : MonoBehaviour
+    {
+        public Sprite silent, speak;
+        public new SpriteRenderer renderer;
+
+        public bool isSpeaking;
+
+        private void Start()
+        {
+            FindObjectOfType<StreamElementsTtsUtterance>().onSpeakFrame += (bool b) => isSpeaking = b;
+        }
+
+        private void Update()
+        {
+            if(isSpeaking)
+            {
+                renderer.sprite = speak;
+            }
+            else
+            {
+                renderer.sprite = silent;
+            }
+        }
+    }
+}

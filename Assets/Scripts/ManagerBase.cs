54f458cf5b8acb1fc5d7c1d7c253a10743452f30
Irish John Games <irishjohngames@gmail.com>
2022-01-26 13:32:59 +0000

    77777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777 _?

---
+++ Assets/Scripts/ManagerBase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Settings
+{
+    public class ManagerBase<T> : MonoBehaviour where T : Component
+    {
+        static T _instance = null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = FindObjectOfType<T>();
+
+                return _instance;
+            }
+        }
+    }
+}
--- Assets/Scripts/TestHarnessCanvasController.cs
+++ Assets/Scripts/TestHarnessCanvasController.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 using System.Linq;
 using CoreTwitchLibSetup;
 
+using System.IO;
+
 public class TestHarnessCanvasController : MonoBehaviour
 {
     [SerializeField] private DisplayMembers dm;
@@ -12,6 +14,8 @@ public class TestHarnessCanvasController : MonoBehaviour
     
     [System.Serializable] private class DisplayMembers
     {
+        [SerializeField] internal Image TestSprite;
+
         [SerializeField] internal InputField SpeakText;
         [SerializeField] internal Button BtnSpeak;
 
@@ -22,11 +26,13 @@ public class TestHarnessCanvasController : MonoBehaviour
         [SerializeField] internal Button BtnTogglePause;
 
         [SerializeField] internal Dropdown Characters;
+
+        [SerializeField] internal Button BtnLoadExtras;
     }
 
     Dictionary<string, StreamElementsTTS_Unity.TtsVoices> VoicesMap = new Dictionary<string, StreamElementsTTS_Unity.TtsVoices>();
 
-    private void Awake()
+    private void Start()
     {
 #if !UNITY_EDITOR
                 gameObject.SetActive(false);
@@ -48,6 +54,56 @@ public class TestHarnessCanvasController : MonoBehaviour
         dm.BtnTogglePause.onClick.AddListener(() => {
             TwitchLibCtrl.TTSPaused = !TwitchLibCtrl.TTSPaused;
         });
+
+        dm.BtnLoadExtras.onClick.AddListener(() =>
+        {
+            foreach (var d in Directory.EnumerateDirectories(Path.Combine(Application.persistentDataPath, "Characters")))
+            {
+                DirectoryInfo di = new DirectoryInfo(d);
+
+                var OpenM = di.GetDirectories().SingleOrDefault(o => o.Name == "Open");
+                var ClosedM = di.GetDirectories().SingleOrDefault(o => o.Name == "Closed");
+
+                var opens = GetSpritesFromDirectory(OpenM);
+                var closeds = GetSpritesFromDirectory(ClosedM);
+                var character = ScriptableObject.CreateInstance<StreamElementsTTS_Unity.TtsCharacter>();
+
+                character.name = di.Name;
+
+                character.silent = closeds.ToArray();
+                character.talk = opens.ToArray();
+
+                Settings.SettingsManager.Instance.dependencies.AllCharacters.Add(character);
+            }
+
+            CharacterChanger();
+        });
+    }
+
+    private List<Sprite> GetSpritesFromDirectory(DirectoryInfo OpenM)
+    {
+        List<Sprite> rtn = new List<Sprite>();
+        if (OpenM != null)
+            foreach (var file in OpenM.EnumerateFiles().Where(o => o.Name.EndsWith("png")))
+            {
+                var tex = LoadPNG(file.FullName);
+                rtn.Add(Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f));
+            }
+        return rtn;
+    }
+
+    Texture2D LoadPNG(string filePath)
+    {
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        }
+        return tex;
     }
 
     private void VoicesChanger()
@@ -71,14 +127,15 @@ public class TestHarnessCanvasController : MonoBehaviour
 
     private void CharacterChanger()
     {
+        dm.Characters.ClearOptions();
+
         List<Dropdown.OptionData> optsForCharactersList = new List<Dropdown.OptionData>();
         foreach (var c in Settings.SettingsManager.Instance.dependencies.AllCharacters) {
             optsForCharactersList.Add(new Dropdown.OptionData(c.name.ToString()));
         }
 
         dm.Characters.AddOptions(optsForCharactersList);
-        dm.Characters.onValueChanged.AddListener(o =>
-        {
+        dm.Characters.onValueChanged.AddListener(o => {
             var y = dm.Characters.options[o].text;
             GameObject.FindObjectOfType<StreamElementsTTS_Unity.TalkingSprite>().m_ActiveCharacter
              = Settings.SettingsManager.Instance.dependencies.AllCharacters.First(t => t.name == y);
